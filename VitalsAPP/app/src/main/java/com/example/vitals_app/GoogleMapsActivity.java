4a693a458fdd15e8b87b971fc2d1fe87047b3591
aadhyaputtur <putturaadhya@gmail.com>
2020-07-25 23:40:54 -0400

    Changes on Camera Position

--- VitalsAPP/app/src/main/java/com/example/vitals_app/GoogleMapsActivity.java
+++ VitalsAPP/app/src/main/java/com/example/vitals_app/GoogleMapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -43,5 +44,13 @@ public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCa
         mMap.addMarker(new MarkerOptions().position(recreationCenter).title("Recreation Center"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(recreationCenter));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(recreationCenter, 18));
+        mMap.setBuildingsEnabled(true);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(recreationCenter)      // Sets the center of the map to Mountain View
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
